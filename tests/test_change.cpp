77ee0634f4dda1478761734f592cc9c3d1133d3d
Никита Ефимов <mr.zhennin@mail.ru>
2025-09-28 21:35:23 +0500

    test: add failing tests for Coin struct

--- tests/test_change.cpp
+++ tests/test_change.cpp
@@ -2,10 +2,9 @@
 #include <vector>
 #include "change.hpp"
 
-
-//1
+// 1
 TEST(ChangeTest, ExactChangePossible) {
-    std::vector<int> coins = {100, 50, 10, 5, 2, 1};
+    std::vector<Coin> coins = {{100,1}, {50,1}, {10,3}, {5,1}, {2,1}, {1,1}};
     std::vector<int> res;
 
     bool result = changeM(186, coins, res);
@@ -19,48 +18,46 @@ TEST(ChangeTest, ExactChangePossible) {
     EXPECT_EQ(res, expected);
 }
 
-//2
+// 2
 TEST(ChangeTest, ChangeNotPossible) {
-    std::vector<int> coins = {5, 3};
+    std::vector<Coin> coins = {{5,1}, {3,1}};
     std::vector<int> res;
 
-    bool result = changeM(2, coins, res);
+    bool result = changeM(6, coins, res);
     EXPECT_FALSE(result);
     EXPECT_TRUE(res.empty());
 }
 
-//3
+// 3
 TEST(ChangeTest, UseAllCoins) {
-    std::vector<int> coins = {5, 3, 1};
+    std::vector<Coin> coins = {{5,1}, {3,1}, {1,1}};
     std::vector<int> res;
 
     bool result = changeM(9, coins, res);
     EXPECT_TRUE(result);
 
-    std::vector<int> expected = {5, 3, 1}; 
+    std::vector<int> expected = {5, 3, 1};
     EXPECT_EQ(res, expected);
 }
 
-//4
-TEST(ChangeTest, ZeroChange)
-{
-   std::vector<int> coins = {1,2,5};
-   std::vector<int> res;
-   
-   bool result = changeM(0, coins, res);
-   EXPECT_TRUE(result);;
-   EXPECT_TRUE(res.empty());
+// 4
+TEST(ChangeTest, ZeroChange) {
+    std::vector<Coin> coins = {{1,5}, {2,3}, {5,2}};
+    std::vector<int> res;
+
+    bool result = changeM(0, coins, res);
+    EXPECT_TRUE(result);
+    EXPECT_TRUE(res.empty());
 }
 
-//5
-TEST(ChangeTest, UnsortedCoins)
-{
-   std::vector<int> coins = {2, 10, 1, 50};
-   std::vector<int> res;
-   
-   bool result = changeM(12, coins, res);
-   EXPECT_TRUE(result);
-   EXPECT_EQ(res, std::vector<int>({10, 2}));
+// 5
+TEST(ChangeTest, UnsortedCoins) {
+    std::vector<Coin> coins = {{2,2}, {10,1}, {1,3}, {50,1}};
+    std::vector<int> res;
+
+    bool result = changeM(12, coins, res);
+    EXPECT_TRUE(result);
+    EXPECT_EQ(res, std::vector<int>({10, 2}));
 }
 
 int main(int argc, char **argv) {
